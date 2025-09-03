71a02707e674bd3fb91a6593c95b861c2d2babcc
Mathias Handeland <127216029+MathiasHandeland@users.noreply.github.com>
2025-09-03 10:41:59 +0200

    feat: clear locally saved theme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,7 +16,8 @@ export default function Header() {
     }
 
     const handleButtonClick = () => {
-      console.log("CLICK!");
+        localStorage.removeItem('theme');
+        setTheme('light');
     }
 
     return (
